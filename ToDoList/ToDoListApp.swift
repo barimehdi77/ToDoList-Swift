b8895d2b06033d811afc9148c06b4a2b5d94b720
Barimehdi77 <barimehdi777@gmail.com>
2024-08-25 20:42:33 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Bari Mehdi77 on 25/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
